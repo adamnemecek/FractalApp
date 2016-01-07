1112a6167f9f43b2cad67d6866edba0912e8876e
tom briggs <tbriggs@cs.ship.edu>
2016-01-07 09:16:07 -0500

    Initial Commit

---
+++ FractalApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FractalApp
+//
+//  Created by Tom Briggs on 1/7/16.
+//  Copyright © 2016 Tom Briggs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
